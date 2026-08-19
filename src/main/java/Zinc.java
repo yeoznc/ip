f0cab0ad5db822d3468050c6493d8570d4e002d7
YeoZn <yeoznc@gmail.com>
2026-08-19 21:19:02 +0800

    Renamed Duke chatbot to Zinc

---
+++ src/main/java/Zinc.java
@@ -0,0 +1,11 @@
+public class Zinc {
+    public static void main(String[] args) {
+        String banner = " ______ _            \n"
+                + "|___  /(_)           \n"
+                + "   / /  _ _ __   ___ \n"
+                + "  / /  | | '_ \\ / __|\n"
+                + " / /___| | | | | (__ \n"
+                + "/______|_|_| |_|\\___|\n";
+        System.out.println(banner);
+    }
+}
